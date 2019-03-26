0f0b9b66a110d1dd4cd791133ca9e7bc279d646e
ysjk2003 <ysjk2003@gmail.com>
2019-03-26 18:35:25 +0900

    [Edit] Make Base Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './info.png';
 import './App.css';
+import Member from './Member';
 
 class App extends Component {
   render() {
@@ -19,12 +20,17 @@ class App extends Component {
         </div>
         <div className="Introduce">
           <img src={logo} className="Logo"/>
-          <div className="Introduce-text">
-            <h2>동아리 소개</h2>
+          <div className="Introduce-club">
+            <h2 className="Introduce-title">동아리 소개</h2>
             <p>INFO 동아리는 대덕소프트웨어마이스터고등학교에서 정보보안에 관심있는 학생들의 Long run을 위한 기반지식 습득 가이드라인을 제작하는 동아리입니다.</p>
           </div>
-          <div>
-            <h2>동아리원 소개</h2>
+          <div className="Introduce-member">
+            <h2 className="Introduce-title">동아리원 소개</h2>
+            <Member />
+          </div>
+          <div className="Introduce-history">
+            <h2 className="Introduce-title">동아리 활동</h2>
+            <p>2017년 동아리 설립</p>
           </div>
         </div>
       </div>
---
+++ src/Member.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class Member extends Component {
+    state = {
+        information: [
+            {
+                id: 0,
+                number: 'DSM2016',
+                name: '이재석'
+            },
+            {
+                id: 0,
+                number: '30414',
+                name: '이지후'
+            },
+            {
+                id: 1,
+                number: 'DSM2016',
+                name: '이재빈'
+            },
+            {
+                id: 1,
+                number: '30401',
+                name: '강민범'
+            },
+            {
+                id: 1,
+                number: '30405',
+                name: '박보현'
+            },
+            {
+                id: 1,
+                number: '20401',
+                name: '김경민'
+            }
+        ]
+    }
+
+    render() {
+        const { information } = this.state;
+        const OperList = information.map(information => (
+            information.id == 0 && (<p>{information.number} {information.name}</p>))
+        );
+        const MemberList = information.map(information => (
+            information.id == 1 && (<p>{information.number} {information.name}</p>)
+        ));
+        return (
+            <div>
+                <h3 className="Introduce-title">운영진</h3>
+                <p>{OperList}</p>
+                <h3 className="Introduce-title">멤버</h3>
+                <p>{MemberList}</p>
+            </div>
+        );
+    }
+}
+
+export default Member;
