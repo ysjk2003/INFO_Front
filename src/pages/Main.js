2a4b8c09b905b58bed9e3aa529c1d1602e419e44
ysjk2003 <ysjk2003@gmail.com>
2019-04-01 19:21:38 +0900

    [Modify] Add media query in Main.css

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -12,9 +12,11 @@ class Main extends Component {
         <Nav />
         <div className="Introduce">
           <div className="Introduce-club">
-            <Link to="/"><img src={info} className="Logo"/></Link>
-            <h2 className="Introduce-title" id="Club-title">동아리 소개</h2>
-            <p>INFO 동아리는 대덕소프트웨어마이스터고등학교에서 정보보안에 관심있는 학생들의 Long run을 위한 기반지식 습득 가이드라인을 제작하는 동아리입니다.</p>
+              <Link to="/"><img src={info} className="Logo"/></Link>
+              <div className="Introduce-description">
+                <h2 className="Introduce-title" id="Club-title">동아리 소개</h2>
+                <p>INFO 동아리는 대덕소프트웨어마이스터고등학교에서 정보보안에 관심있는 학생들의 Long run을 위한 기반지식 습득 가이드라인을 제작하는 동아리입니다.</p>
+              </div>
           </div>
           <div className="Introduce-wrapper">
             <div className="Introduce-member">
