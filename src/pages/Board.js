ae2ca6f63f76d1fd1e2f25d5379674754d58d1a6
ysjk2003 <ysjk2003@gmail.com>
2019-04-01 23:09:16 +0900

    [Add] Board page

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -7,18 +7,23 @@ class Board extends Component {
         return (
             <div>
                 <Nav />
-                <div className="Category">
-                    <h2>Chapter 1</h2>
-                    <p>What is C?</p>
-                </div>
-                <div className="Main">
-                    <h1 className="Title">Title</h1>
-                    <p className="Text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
-                    took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
-                    but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s 
-                    with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
-                    software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <div className="Board-wrapper">
+                    <div className="Board">
+                        <div className="Category">
+                            <h2>Chapter 1</h2>
+                            <p>What is C?</p>
+                        </div>
+                        <div className="Main">
+                            <h1 className="Title">Title</h1>
+                            <p className="Text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
+                            took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+                            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s 
+                            with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+                            software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        </div>
+                    </div>
+                    <button type="submit" className="Create-Button">글쓰기</button>
                 </div>
             </div>
         );
