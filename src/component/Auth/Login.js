5d87f3b0f3d471a598559974d526bfd19143ede3
Kim Min Gyu <ysjk2003@gmail.com>
2019-07-11 15:07:28 +0900

    [Update] Login Component => Container

--- src/component/Auth/Login.js
+++ src/component/Auth/Login.js
@@ -1,71 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Login.css';
 import { Link, withRouter } from 'react-router-dom';
-import axios from 'axios';
-import { setCookie } from 'lib/cookie';
 
-
-class Login extends Component {
-    constructor() {
-        super();
-        this.onClick = this.onClick.bind(this);
-    }
-
-    state = {
-        id: '',
-        password: ''
-    }
-    
-
-    async onClick (e) {
-        e.preventDefault();
-        if (this.state.id === ''){ 
-            alert("아이디를 입력하세요")
-        }
-        else if (this.state.password === '') {
-            alert("비밀번호를 입력하세요")
-        }
-        else {
-            try {
-                const response = await axios.post('http://infodsm.club:5000/account/login', {
-                    id: this.state.id, pw: this.state.password
-                })
-                alert('로그인 성공')
-                this.props.setLogin(true);
-                const logflag = this.props.isLogin;
-                localStorage.setItem('isLogin', logflag)
-                setCookie('JWT', response.data.access_token)
-                this.props.history.push('/')
-            }
-            catch (err) {
-                alert('로그인 실패')
-                console.log(err)
-            }
-        }
-    }
-
-    handleChange = (e) => {
-        this.setState({[e.target.name] : e.target.value})
-    }
-
-    render() {
-        const style = {
-            'textAlign': 'center'
-        }
-        return (
-            <div>
-                <div className="Login">
-                    <h2>로그인</h2>
-                    <form>
-                        <input type="text" name="id" className="Login-box" placeholder="ID" onChange={this.handleChange} required></input><br/>
-                        <input type="password" name="password" className="Login-box" placeholder="Password" onChange={this.handleChange} required></input>
-                        <p style={style}><Link to="/regist">회원가입</Link></p>
-                        <button type="submit" className="Login-submit" onClick={this.onClick}>Login</button>
-                    </form>
-                </div>
+const Login = ({handleChange, onClick}) => {
+    return (
+        <div>
+            <div className="Login">
+                <h2>로그인</h2>
+                <form>
+                    <input type="text" name="id" className="Login-box" placeholder="ID" onChange={handleChange} required></input><br/>
+                    <input type="password" name="password" className="Login-box" placeholder="Password" onChange={handleChange} required></input>
+                    <p><Link to="/regist">회원가입</Link></p>
+                    <button type="submit" className="Login-submit" onClick={onClick}>Login</button>
+                </form>
             </div>
-        );
-    };
+        </div>
+    );
 }
 
 
--- src/container/LoginContainer.js
+++ src/container/LoginContainer.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react'
 import Login from '../component/Auth/Login';
 import * as actions from '../actions';
+import axios from 'axios';
+import { setCookie } from 'lib/cookie';
 import { connect } from 'react-redux';
 
 
 class LoginContainer extends Component {
+
+    state = {
+        id: '',
+        password: ''
+    }
+
+    onClick = async (e) => {
+        e.preventDefault();
+        console.log(this)
+        if (this.state.id === ''){ 
+            alert("아이디를 입력하세요")
+        }
+        else if (this.state.password === '') {
+            alert("비밀번호를 입력하세요")
+        }
+        else {
+            try {
+                const response = await axios.post('http://infodsm.club:5000/account/login', {
+                    id: this.state.id, pw: this.state.password
+                })
+                alert('로그인 성공')
+                this.props.IsLogin(true);
+                const logflag = this.props.isLogin;
+                localStorage.setItem('isLogin', logflag)
+                setCookie('JWT', response.data.access_token)
+                this.props.history.push('/')
+            }
+            catch (err) {
+                alert('로그인 실패')
+                console.log(err)
+            }
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({[e.target.name] : e.target.value})
+    }
+
     render() {
         return (
-            <Login setLogin = {this.props.IsLogin} isLogin = {this.props.isLogin}/>
+            <Login handleChange={this.handleChange} onClick={this.onClick}/>
         )
     }
 }
